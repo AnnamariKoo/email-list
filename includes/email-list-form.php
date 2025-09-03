0825b45080093f9ca26430ab85c353f2af323506
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2025-09-03 15:11:10 +0300

    Pluginiin lisätty html-rakenne

--- includes/email-list-form.php
+++ includes/email-list-form.php
@@ -1,3 +1,9 @@
 <?php
 
-add_shortcode('email-list-form');
+add_shortcode('email-list-form', 'show_email_list_form');
+
+function show_email_list_form() 
+{
+    include MY_PLUGIN_PATH . 'includes/templates/email-list-form.php';
+
+}
---
+++ includes/templates/email-list-form.php
@@ -0,0 +1,18 @@
+<form>
+
+<div class="kokonimi">
+    <label for="etunimi">Etunimi</label>
+    <input type="text" name="etunimi">
+    
+    <label for="sukunimi">Sukunimi</label>
+    <input type="text" name="sukunimi">
+</div>
+
+    <label for="email">Sähköposti</label>
+    <input type="text" name="email">
+
+    <label for="organisaatio">Organisaatio</label>
+    <input type="text" name="organisaatio">
+
+    <button type="submit">Liity postituslistalle</button>
+</form>
