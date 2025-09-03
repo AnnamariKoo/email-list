50ff188dcc268a759a57d572fabcd72205909087
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2025-09-03 14:50:22 +0300

    Sähköpostilistan asetus-sivu valmis

--- includes/options-page.php
+++ includes/options-page.php
@@ -6,21 +6,26 @@ use Carbon_Fields\Container;
 add_action('after_setup_theme', 'load_carbon_fields');
 add_action('carbon_fields_register_fields', 'create_options_page');
 
-function load_carbon_fields()
+function load_carbon_fields(): void
 {
     \Carbon_Fields\Carbon_Fields::boot();
 }
 
-function create_options_page()
+function create_options_page(): void
 {
     Container::make( 'theme_options', __( 'Sähköpostilista asetukset' ) )
-    ->add_fields( array(
-        Field::make( 'text', 'email_list_plugin_recipients', __( 'Vastaanottajan sähköposti' ) ) 
-        ->set_attribute( 'placeholder', 'vastaanottaja@email.com' )
-        ->set_help_text('Sähköpostiosoite, johon lomakkeen tiedot lähetetään'),
-        Field::make( 'textarea', 'email_list_plugin_message', __( 'Vahvistusviesti' ) )
-        ->set_attribute( 'placeholder', 'Kirjoita vahvistusviesti' )
-        ->set_help_text('Vahvistusviesti, joka lähetetään lomakkeen lähettäjälle'),
+        ->set_icon('dashicons-email')
+        ->add_fields( array(
+
+            Field::make( 'checkbox', 'email_list_plugin_active', __( 'Aktivoi sähköpostilista' ) ),
+
+            Field::make( 'text', 'email_list_plugin_recipients', __( 'Vastaanottajan sähköposti' ) ) 
+            ->set_attribute( 'placeholder', 'vastaanottaja@email.com' )
+            ->set_help_text('Sähköpostiosoite, johon lomakkeen tiedot lähetetään'),
+
+            Field::make( 'textarea', 'email_list_plugin_message', __( 'Vahvistusviesti' ) )
+            ->set_attribute( 'placeholder', 'Kirjoita vahvistusviesti' )
+            ->set_help_text('Vahvistusviesti, joka lähetetään lomakkeen lähettäjälle'),
 
     ) );
 }
