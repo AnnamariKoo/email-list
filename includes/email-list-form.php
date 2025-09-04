1c193edac047b8411c1d30a265cc0e05ec88176f
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2025-09-04 09:35:45 +0300

    Pluginia jatkettu eteenpäin testaamalla lähetysnapin toimintaa

--- includes/email-list-form.php
+++ includes/email-list-form.php
@@ -2,8 +2,24 @@
 
 add_shortcode('email-list-form', 'show_email_list_form');
 
+add_action('rest_api_init', 'create_rest_endpoint');
+
 function show_email_list_form() 
 {
     include MY_PLUGIN_PATH . 'includes/templates/email-list-form.php';
 
+}
+
+function create_rest_endpoint()
+{
+    register_rest_route('v1/email-form', 'submit', array(
+
+        'methods' => 'POST',
+        'callback' => 'handle_enquiry',
+    ));
+}
+
+function handle_enquiry()
+{
+     echo 'Hello'; 
 }
--- includes/templates/email-list-form.php
+++ includes/templates/email-list-form.php
@@ -1,12 +1,10 @@
-<form>
+<form id="enquiry_form">
 
-<div class="kokonimi">
     <label for="etunimi">Etunimi</label>
     <input type="text" name="etunimi">
     
     <label for="sukunimi">Sukunimi</label>
     <input type="text" name="sukunimi">
-</div>
 
     <label for="email">Sähköposti</label>
     <input type="text" name="email">
@@ -16,3 +14,22 @@
 
     <button type="submit">Liity postituslistalle</button>
 </form>
+
+<script>
+
+    // jQuery(document).ready(function($){
+
+    //     $("#enquiry_form").submit( function(event){
+
+    //         alert('test')
+
+    //     });
+    // });
+    document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('enquiry_form').addEventListener('submit', function(event) {
+        alert('test');
+        // event.preventDefault(); // Uncomment if you want to prevent form submission
+    });
+});
+
+</script>
